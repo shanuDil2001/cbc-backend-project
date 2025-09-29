9a5c58097a216fc6e4e01e58614693eb22342833
Shanuka <shanukadilshan.swe@gmail.com>
2025-09-29 16:19:54 +0530

    Initialize the repository and create the basic setup of the back-end project ✏

---
+++ index.js
@@ -0,0 +1,14 @@
+// Section 01: Import dependencies
+import express from "express";
+
+// Section 02: Create an express app
+const app = express();
+
+// Section 03: Middleware configurations
+
+// Section 04: Define routes
+
+// Section 05: Start the server
+app.listen(5000, () => {
+   console.log("Server is running on port: 5000");
+});
