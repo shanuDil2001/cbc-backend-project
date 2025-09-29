1eb4b452c28a2d448ed6509af1bfc29d875c3c50
Shanuka <shanukadilshan.swe@gmail.com>
2025-09-29 19:18:23 +0530

    Created necessary things for establishing a connection with MongoDB using mongoose ✏

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 // Section 01: Import dependencies
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load .env file into process.env object
+
+
 
 // Section 02: Create an express app
 const app = express();
 
+
+
 // Section 03: Middleware configurations
 
+
+
+// MongoDB Connection
+try {
+   mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+   console.log("Connection to MongoDB was established.");
+} catch (error) {
+   console.log("Connection to MongoDB was failed.");
+   console.error(error);
+}
+
+
+
 // Section 04: Define routes
+
+
 
 // Section 05: Start the server
 app.listen(5000, () => {
