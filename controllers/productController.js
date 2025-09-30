20e0e5b10163d87d978decc1bcf12332822bccc4
Shanuka <shanukadilshan.swe@gmail.com>
2025-09-30 16:53:40 +0530

    Modified createProduct function. ✏

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,18 +1,25 @@
 import Product from "../models/productModel.js";
+import { isAdmin } from "./userController.js";
 
 export async function createProduct(req, res) {
-   const newProduct = new Product(req.body);
+   if (!isAdmin(req)) {
+      return res.status(401).json({
+         message: "You are not authorized to create products."
+      });
+   } else {
+      const newProduct = new Product(req.body);
 
-   try {
-      const savedProduct = await newProduct.save();
-      res.status(201).json({
-         message: "Product created successful.",
-         product: savedProduct.productId
-      });
-   } catch (error) {
-      res.status(500).json({
-         message: "Failed to create the product."
-      });
-      console.error(error);
+      try {
+         const savedProduct = await newProduct.save();
+         res.status(201).json({
+            message: "Product created successful.",
+            product: savedProduct.productId
+         });
+      } catch (error) {
+         res.status(500).json({
+            message: "Failed to create the product."
+         });
+         console.error(error);
+      }
    }
 }
