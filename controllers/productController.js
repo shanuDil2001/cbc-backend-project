95fb8e0d462630f7f392687c1c9b7796672867d5
Shanuka <shanukadilshan.swe@gmail.com>
2025-09-30 18:54:23 +0530

    Implemented getProduct and getProducts functions for getting products. ✏

--- controllers/productController.js
+++ controllers/productController.js
@@ -23,3 +23,70 @@ export async function createProduct(req, res) {
       }
    }
 }
+
+export async function getProduct(req, res) {
+   const productId = req.params.productId;
+
+   const product = await Product.findOne({ productId: productId });
+
+   if (!isAdmin(req)) {
+
+      if (product != null) {
+         if (product.isAvailable == false) {
+            return res.status(404).json({
+               message: "Product not found."
+            });
+         } else {
+            return res.status(200).json(product);
+         }
+      } else {
+         return res.status(404).json({
+            message: "Product not found."
+         });
+      }
+   } else {
+      if (product != null) {
+         return res.status(200).json(product);
+      } else {
+         return res.status(404).json({
+            message: "Product not found."
+         });
+      }
+   }
+}
+
+export async function getProducts(req, res) {
+   if (!isAdmin(req)) {
+      try {
+         const products = await Product.find({ isAvailable: true });
+
+         return res.status(200).json(products);
+      } catch (error) {
+         console.error(error);
+
+         return res.status(500).json({
+            message: "Failed to get products."
+         });
+      }
+   } else {
+      try {
+         const products = await Product.find();
+
+         return res.status(200).json(products);
+      } catch (error) {
+         console.error(error);
+
+         return res.status(500).json({
+            message: "Failed to get products."
+         });
+      }
+   }
+}
+
+export async function updateProduct(req, res) {
+
+}
+
+export async function deleteProduct(req, res) {
+
+}
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createProduct } from "../controllers/productController.js";
+import { createProduct, getProduct, getProducts } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/", createProduct);
+productRouter.get("/", getProducts);
+productRouter.get("/:productId", getProduct);
 
 export default productRouter;
